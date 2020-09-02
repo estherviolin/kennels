0be175bef8ad3d579b4f1b4c6408686b43c8b2b8
estherviolin <esthernaomi.violin@gmail.com>
2020-09-02 14:41:33 -0500

    location provider

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect } from "react"
+import { LocationContext } from "./LocationProvider"
+
+export const LocationList = () => {
+    const { locations, getLocations } = useContext(LocationContext) //imported from provider
+
+    useEffect(() => {
+        getLocations()
+    }, [])
+
+    useEffect(() => {
+    }, [locations])
+
+    return (
+        <article className="locations">
+            {
+                locations.map(location => {
+                    return <section key={location.id} className="location">
+                                <div><h3>{location.name}</h3></div>
+                                <div>{location.address}</div>
+                            </section>
+                })
+            }
+
+
+        </article>
+    )
+}
