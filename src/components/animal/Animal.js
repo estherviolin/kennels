2ca4363fa898967a216c897faa0d0153475e99b2
estherviolin <esthernaomi.violin@gmail.com>
2020-09-03 15:31:30 -0500

    animals working

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,10 +1,10 @@
 import React from "react"
 import "./Animals.css"
 
-export const Animal = ({animal}) => (
+export const Animal = ({animal, customer, location}) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__owner">Owner: {animal.customerId}</div>
-        <div className="animal__location">Location: {animal.locationId}</div>
+        <div className="animal__owner">Owner: {customer.name}</div>
+        <div className="animal__location">Location: {location.name}</div>
     </section>
 )
