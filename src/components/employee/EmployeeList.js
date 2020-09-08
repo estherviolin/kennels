ec19b9b2436574b0d14f6d3bad51e73bc6664e8f
estherviolin <esthernaomi.violin@gmail.com>
2020-09-08 11:12:31 -0500

    login and location string rendering

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -1,31 +1,36 @@
 import React, { useContext, useEffect } from "react"
 import { EmployeeContext } from "./EmployeeProvider"
-import {Employee} from "./Employee"
+import { Employee } from "./Employee"
 import "./Employee.css"
+import { LocationContext } from "../location/LocationProvider"
 
 
 export const EmployeeList = (props) => {
     const { employees, getEmployees } = useContext(EmployeeContext) //imported from provider
+    const { locations, getLocations } = useContext(LocationContext)
 
     useEffect(() => {
         getEmployees()
+        getLocations()
     }, [])
 
-    useEffect(() => {
-    }, [employees])
 
     return (
         <div className="employees">
             <h1>Employees</h1>
-            <button onClick = {() => props.history.push("/employees/create")}>
+            <button onClick={() => props.history.push("/employees/create")}>
                 Add Employee
             </button>
             <article className="employeeList">
                 {
-                    employees.map(employee => <Employee key={employee.id} employee={employee} />)
-                }       
+                    employees.map(employee => {
+                        const clinic = locations.find(location => location.id === employee.locationId) || {}
+
+                        return <Employee key={employee.id} employee={employee} location={clinic} />
+                    })
+                }
             </article>
-          
+
         </div>
     )
 }
